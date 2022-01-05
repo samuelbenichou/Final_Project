08d5ab3c09f878c2eeb195a4f255189bb5d0b3da
tom dugma <dugma@post.bgu.ac.il>
2022-01-05 19:12:47 +0200

    added main

--- Model/Simulation/run_app.py
+++ Model/Simulation/run_app.py
@@ -1,4 +1,7 @@
+import sys
+import data_feature_sub
 import numpy as np
+import pandas as pd
 from skmultiflow.data import FileStream
 from skmultiflow.neural_networks import PerceptronMask
 from sklearn.metrics import accuracy_score
@@ -9,9 +12,11 @@ from Model.OFS import fires as fires
 from docx import Document
 import timeit
 import os
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def apply_fires(df_name, tgt_index, epochs=1, batch_sizes = [25, 50, 75, 100], fractions = [0.1, 0.15, 0.2]):
+
+def apply_fires(df_name, tgt_index, epochs=1, batch_sizes=[25, 50, 75, 100], fractions=[0.1, 0.15, 0.2]):
     final_stab_lst = []
     final_acc_lst = []
     document = Document()
@@ -34,19 +39,19 @@ def apply_fires(df_name, tgt_index, epochs=1, batch_sizes = [25, 50, 75, 100], f
 
             # Initialize FIRES
             fires_model = fires.FIRES(n_total_ftr=stream.n_features,  # Total no. of features
-                                target_values=stream.target_values,  # Unique target values (class labels)
-                                mu_init=0,  # Initial importance parameter
-                                sigma_init=1,  # Initial uncertainty parameter
-                                penalty_s=0.01,
-                                # Penalty factor for the uncertainty (corresponds to gamma_s in the paper)
-                                penalty_r=0.01,
-                                # Penalty factor for the regularization (corresponds to gamma_r in the paper)
-                                epochs=epochs,
-                                # No. of epochs that we use each batch of observations to update the parameters
-                                lr_mu=0.01,  # Learning rate for the gradient update of the importance
-                                lr_sigma=0.01,  # Learning rate for the gradient update of the uncertainty
-                                scale_weights=True,  # If True, scale feature weights into the range [0,1]
-                                model='probit')  # Name of the base model to compute the likelihood
+                                      target_values=stream.target_values,  # Unique target values (class labels)
+                                      mu_init=0,  # Initial importance parameter
+                                      sigma_init=1,  # Initial uncertainty parameter
+                                      penalty_s=0.01,
+                                      # Penalty factor for the uncertainty (corresponds to gamma_s in the paper)
+                                      penalty_r=0.01,
+                                      # Penalty factor for the regularization (corresponds to gamma_r in the paper)
+                                      epochs=epochs,
+                                      # No. of epochs that we use each batch of observations to update the parameters
+                                      lr_mu=0.01,  # Learning rate for the gradient update of the importance
+                                      lr_sigma=0.01,  # Learning rate for the gradient update of the uncertainty
+                                      scale_weights=True,  # If True, scale feature weights into the range [0,1]
+                                      model='probit')  # Name of the base model to compute the likelihood
 
             # Variables for calculating the average accuracy and stability per time step
             n_selected_ftr = round(frac_selected_ftr * stream.n_features)
@@ -126,5 +131,103 @@ def apply_fires(df_name, tgt_index, epochs=1, batch_sizes = [25, 50, 75, 100], f
     print(f'Final avg acc score: {sum(final_acc_lst) / len(final_acc_lst)}')
     print(f'Final avg stab score: {sum(final_stab_lst) / len(final_stab_lst)}')
 
+
+def msg(data, chunk_size, feature_precent=0.0,varying=False,trapizodal=False):
+    while True:
+        OFS_choose = input(
+            "Please choose an Online Feature Selection (OFS) algorithm: \n for FIRES press 1 \n for ABFS press 2 \n "
+            "for MC-NN press 3")
+        if OFS_choose == '1':
+            try:
+                tgt_index = int(input("Please enter your index target label (the classifier)"))
+                fraction = int(input("Please enter a fraction value"))
+                feature_count = data_feature_sub.get_number_of_features_by_percentage(data, feature_precent)
+                for i in range(chunk_size, len(data), chunk_size):
+                    sub_data = data_feature_sub.select_sub_data_from_data(data, i)
+                    if varying:
+                        sub_data = data_feature_sub.select_feature_set_from_data(sub_data,feature_count)
+                    elif trapizodal:
+                        sub_data = data_feature_sub.expand_feature_set_from_data(sub_data,feature_count)
+                    apply_fires(sub_data, tgt_index, fraction)
+            except Exception as e:
+                print(e)
+
+        elif OFS_choose == '2':  # ABFS
+            pass
+
+        elif OFS_choose == '3':  # MC-NN
+            pass
+
+        else:
+            sys.stdout.write("invalid input, please try agian")
+
+        # OL_choose = input(
+        #     "Please choose an Online Learning (OL) algorithm: \n for KNNClassifier press 1 \n for PerceptronMask press 2 \n for NaiveBayes press 3 \n for AdaptiveRandomForestClassifier press 4")
+        # try:
+        #     if OL_choose == '1':
+        #         n_neighbors = int(input("Please enter number of neighbors value"))
+        #         max_window_size = int(input("Please enter max window size value"))
+        #         leaf_size = int(input("Please enter leaf size value"))
+        #         metric = "euclidean"
+        #
+        #     elif OL_choose == '2':
+        #         alpha = int(input("Please enter alpha value"))
+        #         max_iter = int(input("Please enter max iteration value"))
+        #
+        #     elif OL_choose == '3':  #TODO what are naive baise params?
+        #         pass
+        #
+        #     elif OL_choose == '4':
+        #         n_estimators = int(input("Please enter number of estimators value"))
+        #         lambda_value = int(input("Please enter lambda value"))
+        #         split_confidence = int(input("Please enter split confidence value"))
+        #         tie_threshold = int(input("Please enter tie_threshold value"))
+        #         performance_metric = "acc"
+        #         split_criterion = "info_gain"
+        #
+        #     else:
+        #         sys.stdout.write("invalid input, please try agian")
+        # except Exception as e:
+        #     print(e)
+
+        # OFS_choose & OL_choose
+
+
 if __name__ == "__main__":
-    apply_fires(df_name='file_path', tgt_index=0, epochs=1)
+    # apply_fires(df_name='file_path', tgt_index=0, epochs=1)
+    while True:
+        data_input = input("Please insert csv url path")
+        try:
+            data = pd.read_csv(data_input)
+        except Exception as e:
+            print(e)
+            continue
+        choice = input(
+            "Please press 1 if you want stream. \n press 2 if you want batch.")
+
+        if choice == '1':  # stream
+            feature_choice = input(
+                "Please press 1 if you want to run regular datastream \n press 2 if you want to use varying feature space \n press 3 for trapizodal.")
+            chunk_size = input("enter chunk size")
+            if feature_choice == '1':
+                msg(data, chunk_size)
+                pass
+
+            elif feature_choice == '2':
+                feature_precent = float(input("Choose feature % to randomize every time Chunk size (for example 0.5, 0.1 etc..)"))
+                msg(data, chunk_size, feature_precent,varying=True)
+
+            elif feature_choice == '3':
+                feature_precent = float(input("Choose feature % to add every time Chunk size (for example 0.5, 0.1 etc..)"))
+                msg(data, chunk_size, feature_precent,trapizodal=True)
+
+            else:
+                sys.stdout.write("invalid input, please try again")
+                continue
+
+        elif choice == '2':  # shuffle from data, working on all features
+            batch_size = int(input("enter batch size"))
+            msg(data)
+
+        else:  # bad key press
+            continue
